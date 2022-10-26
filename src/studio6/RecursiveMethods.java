255976e3ea1e2174ee649de790e5a8b0cdaadb3b
1230e <1230e@DESKTOP-EN6GDDG.dhcp.wustl.edu>
2022-10-26 14:50:42 -0500

    hi

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -14,7 +14,12 @@ public class RecursiveMethods {
 	public static double geometricSum(int n) {
 		
 			// FIXME compute the geometric sum for the first n terms recursively
-			return 0;
+			if (n == 0) {
+				return 0;
+			}
+			else {
+				return geometricSum(n-1) + Math.pow(0.5, n);
+			}
 		
 	}
 
